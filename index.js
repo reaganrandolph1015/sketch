78a58a19bffcb217a11fc032beaf01e7b27a6d40
Reagan <reaganrandolph1015@gmail.com>
2022-01-14 16:57:13 -0600

    Added mouseover event to grid cells for tracing

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+// constants to select element
 const container = document.querySelector('#container');
 
+// function for making grid
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (let c = 0; c < (rows * cols); c++) {
         let cell = document.createElement('div');
+        cell.id = 'cell_id';
+        // function for color change on mouseover
+        cell.addEventListener("mouseover", e => 
+        // highlights on mouseover
+        e.target.classList.add('gridBack')
+        );
         container.appendChild(cell).className = "gridItem";
     };
 };
-
+// makes 16x16 grid
 makeRows(16, 16)

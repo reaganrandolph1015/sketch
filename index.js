8431213d0db2f7a7d9e519f2ba8979bddb3825a9
Reagan <reaganrandolph1015@gmail.com>
2022-01-15 12:25:28 -0600

    Got hover working with old method

--- index.js
+++ index.js
@@ -9,13 +9,18 @@ function makeGrid(rows, cols) {
     for (let c = 0; c < (rows * cols); c++) {
         let cell = document.createElement('div');
         cell.id = 'cell_id';
+        // function for color change on mouseover
+        cell.addEventListener("mouseover", e => 
+        // highlights on mouseover
+        e.target.classList.add('gridBack')
+        );
         container.appendChild(cell).className = "gridItem";
     };
 };
 
 // function for color change on mouseover
 boxes.forEach(gridItem => {
-    gridItem.addEventListener("mouseover", event => {
+    gridItem.addEventListener("mouseover", () => {
 // highlights on mouseover
         gridItem.style.backgroundColor = "red";
     })

85bfa72f5c16945ce08f9e1bfa6e311fc1f77f51
Reagan <reaganrandolph1015@gmail.com>
2022-01-14 19:25:24 -0600

    Working on reset button and new grid method

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 // constants to select element
 const container = document.querySelector('#container');
-
+const button = document.getElementById("button");
+const boxes = document.querySelectorAll('.gridItem');
 // function for making grid
-function makeRows(rows, cols) {
+function makeGrid(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (let c = 0; c < (rows * cols); c++) {
         let cell = document.createElement('div');
         cell.id = 'cell_id';
-        // function for color change on mouseover
-        cell.addEventListener("mouseover", e => 
-        // highlights on mouseover
-        e.target.classList.add('gridBack')
-        );
         container.appendChild(cell).className = "gridItem";
     };
 };
+
+// function for color change on mouseover
+boxes.forEach(gridItem => {
+    gridItem.addEventListener("mouseover", event => {
+// highlights on mouseover
+        gridItem.style.backgroundColor = "red";
+    })
+});
+
+// function for game reset on button click
+button.addEventListener("click", () => {
+    boxes.forEach(gridItem => {
+        gridItem.style.backgroundColor = "white";
+    });
+});
 // makes 16x16 grid
-makeRows(16, 16)
+makeGrid(16, 16)
